d81b32b5639296fccd60dc5ee02a9405e2984dcd
ColonelCrackers <epadikkala@gmail.com>
2017-04-24 18:28:35 -0400

    Initial commit

---
+++ app/src/main/java/cpsproject/androidstudiosucks/MainActivity.java
@@ -0,0 +1,22 @@
+package cpsproject.androidstudiosucks;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        /*EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);*/
+        startActivity(intent);
+    }
+}
