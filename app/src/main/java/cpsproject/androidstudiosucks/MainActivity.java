16cbc7d304fc358c96a42359edb8e80faa7e172f
ColonelCrackers <epadikkala@gmail.com>
2017-05-07 22:29:43 -0400

    New server implementation

--- app/src/main/java/cpsproject/androidstudiosucks/MainActivity.java
+++ app/src/main/java/cpsproject/androidstudiosucks/MainActivity.java
@@ -1,147 +1,116 @@
 package cpsproject.androidstudiosucks;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v4.app.TaskStackBuilder;
-import android.support.v7.app.NotificationCompat;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
-public class MainActivity extends Activity
-{
-    ConnectionClass connectionClass;
-    EditText edtuserid,edtpass;
-    Button btnlogin;
-    ProgressBar pbbar;
-    //android:icon="@android:drawable/ic_menu_save";
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextViewReplyFromServer;
+    private EditText mEditTextSendMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        connectionClass = new ConnectionClass();
-        edtuserid = (EditText) findViewById(R.id.et_username);
-        edtpass = (EditText) findViewById(R.id.et_password);
-        btnlogin = (Button) findViewById(R.id.btn_Login);
-        pbbar = (ProgressBar) findViewById(R.id.pbbar);
-        pbbar.setVisibility(View.GONE);
+        Button buttonSend = (Button) findViewById(R.id.btn_send);
 
-        btnlogin.setOnClickListener(new View.OnClickListener() {
+        mEditTextSendMessage = (EditText) findViewById(R.id.edt_send_message);
+        mTextViewReplyFromServer = (TextView) findViewById(R.id.tv_reply_from_server);
+
+        buttonSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.btn_send:
+                sendMessage(mEditTextSendMessage.getText().toString());
+                try{
+                    Context context = getApplicationContext();
+                    Socket c = new Socket("137.140.181.126", 22);
+                    String x = String.valueOf(c.isConnected());
+                    System.out.println(c.isConnected());
+                    Toast toast = Toast.makeText(context, x,Toast.LENGTH_LONG);
+                    toast.show();
+                    c.close();
+                } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+                break;
+        }
+    }
+
+    private void sendMessage(final String msg) {
+
+        final Handler handler = new Handler();
+        Thread thread = new Thread(new Runnable() {
             @Override
-            public void onClick(View v) {
-                DoLogin  doLogin = new DoLogin();
-                doLogin.execute("");
-                MainActivity.this.openSettings(v); // bypasses server connection
-                //btnlogin.openSettings();
+            public void run() {
 
+                try {
+                    //Replace below IP with the IP of that device in which server socket open.
+                    //If you change port then change the port number in the server side code also.
+                    Context context = getApplicationContext();
+                    Socket s = new Socket("137.140.183.226", 22);
+                    String x = String.valueOf(s.isConnected());
+                    Toast toast = Toast.makeText(context, x,Toast.LENGTH_LONG);
+                    toast.show();
+                    OutputStream out = s.getOutputStream();
+
+                    PrintWriter output = new PrintWriter(out);
+
+                    output.println(msg);
+                    output.flush();
+                    BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                    final String st = input.readLine();
+
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            String s = mTextViewReplyFromServer.getText().toString();
+                            if (st.trim().length() != 0)
+                                mTextViewReplyFromServer.setText(s + "\nFrom Server : " + st);
+                        }
+                    });
+
+                    output.close();
+                    out.close();
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+        thread.start();
     }
-
-    public class DoLogin extends AsyncTask<String,String,String>
-    {
-        String z = "";
-        Boolean isSuccess = false;
-
-        String userid = edtuserid.getText().toString();
-        String password = edtpass.getText().toString();
-
-        @Override
-        protected void onPreExecute() {
-            pbbar.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected void onPostExecute(String r) {
-            pbbar.setVisibility(View.GONE);
-            Toast.makeText(MainActivity.this,r,Toast.LENGTH_SHORT).show();
-
-            if(isSuccess) {
-                Toast.makeText(MainActivity.this,r,Toast.LENGTH_SHORT).show();
-            }
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            if(userid.trim().equals("")|| password.trim().equals(""))
-                z = "Please enter User Id and Password";
-            else
-            {
-                try {
-                    Connection con = connectionClass.CONN();
-                    if (con == null) {
-                        z = "Error in connection with SQL server";
-                        isSuccess = true; // hardcoded to bypass server connection
-                    } else {
-                        String query = "select * from pathologic_app_users where username='" + userid + "' and password='" + password + "'";
-                        Statement stmt = con.createStatement();
-                        ResultSet rs = stmt.executeQuery(query);
-                        if(rs.next())
-                        {
-                            z = "Login successfull";
-                            isSuccess=true;
-                        }
-                        else
-                        {
-                            z = "Invalid Credentials";
-                            isSuccess = false;
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    isSuccess = false;
-                    z = "Exceptions";
-                }
-            }
-            return z;
-        }
-    }
-
+    
     public void openSettings(View view) {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
-    /*public void notifTest(){
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this).setSmallIcon(R.drawable.notification_icon)
-                .setContentTitle("My notification").setContentText("Hello World!");
-    // Creates an explicit intent for an Activity in your app
-        Intent resultIntent = new Intent(this, ResultActivity.class);
 
-    // The stack builder object will contain an artificial back stack for the
-    // started Activity.
-    // This ensures that navigating backward from the Activity leads out of
-    // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-    // Adds the back stack for the Intent (but not the Intent itself)
-        stackBuilder.addParentStack(ResultActivity.class);
-    // Adds the Intent that starts the Activity to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(
-                        0,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-        mBuilder.setContentIntent(resultPendingIntent);
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-        //mNotificationManager.notify(mId, mBuilder.build());
-    }*/
 
 }
 
